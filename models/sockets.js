c7e2942d36074f6675c240c91a74e9f154eb6c58
EdwardR0507 <ramosedward21@gmail.com>
2022-04-09 01:11:46 -0500

    feat: crud complete

--- models/sockets.js
+++ models/sockets.js
@@ -11,6 +11,26 @@ class Sockets {
       console.log("New client connected");
       // Emit all groups to client
       socket.emit("current-groups", this.groupList.getGroups());
+      // Recieve votes
+      socket.on("vote", (id) => {
+        this.groupList.incrementVotes(id);
+        this.io.emit("current-groups", this.groupList.getGroups());
+      });
+      // Add group
+      socket.on("add-group", (name) => {
+        this.groupList.addGroup(name);
+        this.io.emit("current-groups", this.groupList.getGroups());
+      });
+      // Remove group
+      socket.on("remove", (id) => {
+        this.groupList.removeGroup(id);
+        this.io.emit("current-groups", this.groupList.getGroups());
+      });
+      // Update name
+      socket.on("update-name", ({ id, name }) => {
+        this.groupList.updateName(id, name);
+        this.io.emit("current-groups", this.groupList.getGroups());
+      });
     });
   }
 }
